29800983bf03ab29b8ee251fd1be947c0eaf91f4
Jxhnn95 <johnny952203@gmail.com>
2023-06-15 09:25:14 +0200

    enviados y recibidos

---
+++ src/app/componentes/componentes.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TablaTransferenciasComponent } from './tabla-transferencias/tabla-transferencias.component';
+
+
+
+@NgModule({
+  declarations: [
+    TablaTransferenciasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+
+  imports:{
+    TablaTransferenciasComponent
+  }
+})
+export class ComponentesModule { }
